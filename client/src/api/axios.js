3573795519c630b06be74ac28ec1bbb6ee1b0af7
kmthehippie <66264446+kmthehippie@users.noreply.github.com>
2024-06-05 16:21:12 +0800

    Axios update BASE_URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:3000/"
+const BASE_URL = import.meta.env.BASE_URL
 //base url is the port available on express
 
 export default axios.create({
