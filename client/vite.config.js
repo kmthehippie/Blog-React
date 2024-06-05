07f9f168f76ea8a13b5ee106d06860f3dc30265b
kmthehippie <66264446+kmthehippie@users.noreply.github.com>
2024-06-05 18:10:49 +0800

    Update vite config details

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
+import copy from 'rollup-plugin-copy';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA(),
+    copy({
+      targets: [
+        { src: 'public/_redirects', dest: 'dist' } // Ensure this line is added
+      ]
+    })
+  ],
+  build: {
+    outDir: 'dist',
+  },
+});
