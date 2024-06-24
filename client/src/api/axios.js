30392f2de4dd33955c636c8ba656bc5dbac2fc5e
kmthehippie <66264446+kmthehippie@users.noreply.github.com>
2024-06-24 22:14:49 +0800

    Update axios for publishing

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios"
 
-// const BASE_URL = "https://km-blog-server-2024.fly.dev"
-//base url is the port available on express
-const BASE_URL = "http://localhost:3000/"
+const BASE_URL = "https://km-blog-server-2024.fly.dev"
+// base url is the port available on express
+// const BASE_URL = "http://localhost:3000/"
+
 export default axios.create({
     baseURL : BASE_URL,
 })
